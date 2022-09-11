2c6e8ef78b0ffb0b398f76a062640e22432a3a37
IgorDoring <idoring@tutanota.com>
2022-09-10 21:52:52 -0300

    progresso com a logica do jogo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TabuleiroComponent } from './tabuleiro/tabuleiro.component';
-import { TecladoDeLetrasComponent } from './teclado-de-letras/teclado-de-letras.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TabuleiroComponent,
-    TecladoDeLetrasComponent
+    TabuleiroComponent
   ],
   imports: [
     BrowserModule,
--- src/app/tabuleiro/tabuleiro.component.ts
+++ src/app/tabuleiro/tabuleiro.component.ts
@@ -1,37 +1,83 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-tabuleiro',
-  templateUrl: './tabuleiro.component.html',
-  styleUrls: ['./tabuleiro.component.css']
+	selector: 'app-tabuleiro',
+	templateUrl: './tabuleiro.component.html',
+	styleUrls: ['./tabuleiro.component.css']
 })
 
 export class TabuleiroComponent implements OnInit {
 
-  linha: number = 0;
-  coluna: number = 0;
+	linha: number = 0;
+	coluna: number = 0;
+	palavraUser: string = "";
+	palavraAdivinhar: string = "ASSAR";
+	ganhou: number = 0;
 
-  tabuleiro: any[][] = [
-    [{letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}],
-    [{letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}],
-    [{letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}],
-    [{letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}],
-    [{letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}],
-    [{letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}]
-  ];
+	tabuleiro: any[][] = [
+		[{letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}],
+		[{letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}],
+		[{letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}],
+		[{letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}],
+		[{letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}],
+		[{letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}]
+	];
 
-  montarIndex(linha: any, coluna: any): string {
-    return linha + "-" + coluna;
-  }
+	montarIndex(linha: any, coluna: any): string {
+		return linha + "-" + coluna;
+	}
 
-  getLetra(letra: string) : void {
-    this.tabuleiro[this.linha][this.coluna].letra = letra;
-  }
+	getLetra(letra: string) : void {
+		if (this.coluna<5&&this.linhas<6){
+			this.tabuleiro[this.linha][this.coluna].letra = letra;
+			this.coluna += 1;
+			this.palavraUser = this.palavraUser + letra.toUpperCase();
+		}
+	}
 
-  constructor() { }
+	apagarLetra(): void{
+		if(this.coluna > 0&&this.linhas<6){
+			this.coluna -= 1;
+			this.tabuleiro[this.linha][this.coluna].letra = "";
+			this.palavraUser = this.palavraUser.slice(0, this.coluna);
+		}
+	}
 
-  ngOnInit(): void {
-  }
+	adivinhar(): void {
+			for (let j = 0; j < 5; j++){
+				if (this.palavraUser.charAt(j) == this.palavraAdivinhar.charAt(j)){
+					this.tabuleiro[this.linha][j].classe = "acerto";
+					this.ganhou++;
+					continue;
+				}
+				if(this.palavraUser.charAt(j) != this.palavraAdivinhar.charAt(j)&&
+					this.palavraAdivinhar.indexOf(this.palavraUser.charAt(j)) > -1){
+					this.tabuleiro[this.linha][j].classe = "lugarErrado";
+					continue;
+				}
+				if(this.palavraUser.charAt(j) != this.palavraAdivinhar.charAt(j)&&
+					this.palavraAdivinhar.indexOf(this.palavraUser.charAt(j)) == -1){
+					this.tabuleiro[this.linha][j].classe = "erro";
+					continue;
+				}
+			}
+			if(this.ganhou==5){
+				alert("ganhou");
+			}else{
+				if(this.linhas==5){
+					alert("todas as tentativas usadas");
+				}else{
+					this.ganhou = 0;
+					this.linha += 1;
+					this.coluna = 0;
+					this.palavraUser = "";
+				}
+			}
+	}
 
+	constructor() { }
 
+	ngOnInit(): void {
+
+	}
 }
