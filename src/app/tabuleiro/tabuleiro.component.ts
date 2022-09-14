db6b76c7c3a90dbf2da57e0c286e2c7871bae296
IgorDoring <idoring@tutanota.com>
2022-09-14 01:17:08 -0300

    Refatoração lógica do tabuleiro parte 2

--- src/app/tabuleiro/tabuleiro.component.ts
+++ src/app/tabuleiro/tabuleiro.component.ts
@@ -28,7 +28,7 @@ export class TabuleiroComponent implements OnInit {
 	}
 
 	getLetra(letra: string) : void {
-		if (this.coluna<5&&this.linha<6){
+		if (this.coluna<5){
 			this.tabuleiro[this.linha][this.coluna].letra = letra;
 			this.coluna += 1;
 			this.palavraUser = this.palavraUser + letra.toUpperCase();
@@ -36,7 +36,7 @@ export class TabuleiroComponent implements OnInit {
 	}
 
 	apagarLetra(): void{
-		if(this.coluna > 0&&this.linha<6){
+		if(this.coluna > 0&&this.linha < 6){
 			this.coluna -= 1;
 			this.tabuleiro[this.linha][this.coluna].letra = "";
 			this.palavraUser = this.palavraUser.slice(0, this.coluna);
@@ -44,68 +44,45 @@ export class TabuleiroComponent implements OnInit {
 	}
 
 	adivinhar(tentativa: string[] = this.palavraUser.split(""), resposta: string[] = this.palavraAdivinhar.split("")): void {
-		if(this.coluna == 6){
+		console.log(this.linha);
+		if(this.coluna < 5){
 			this.mensagem = "É obrigatório conter 5 letras.";
 			return;
 		}
 		let ganhou: number = 0;
 		// inicia o loop
 		for(let i = 0; i < 5; i++){
-			if(tentativa[i]==resposta[i]){
-				this.tabuleiro[this.linha][i].classe = "acerto";
-				ganhou++;
-			}else if(resposta.join("").indexOf(tentativa[i], i) > -1){
-				this.tabuleiro[this.linha][i].classe = "lugarErrado";
-				resposta[resposta.join("").indexOf(tentativa[i]), i] = "";
+			//checa se eh a mesma letra na mesma posicao
+			console.log("tentativa: "+tentativa.join("")+" | resposta: "+resposta.join(""));
+			console.log(i+" Letra tentativa = "+ tentativa[i]+" | Letra resposta = "+ resposta[i]);
+			if(tentativa[i]==resposta[i]){ // se sim
+				this.tabuleiro[this.linha][i].classe = "acerto";//classifica atributo classe do objeto como acerto
+				ganhou++;//acumula um condição de vitória
+				console.log(i+" Letra tentativa = "+ tentativa[i]+" | Letra resposta = "+ resposta[i]+" --- Acerto")
+				resposta[i] = " ";
 			}else{
-				this.tabuleiro[this.linha][i].classe = "erro";
+				if(resposta.join("").indexOf(tentativa[i]) > -1){//se nao é, mas a letra existe na palavra
+					this.tabuleiro[this.linha][i].classe = "lugarErrado";//classifica a letra atual como no lugar errado
+					console.log(i+" Letra tentativa = "+ tentativa[i]+" | Letra resposta = "+ resposta.join("").indexOf(tentativa[i])+" --- Lugar Errado")
+					resposta[resposta.join("").indexOf(tentativa[i])] = "";//descobre o index 
+				}else{
+					console.log(i+" Letra tentativa = "+ tentativa[i]+" | Letra resposta = "+ resposta.join("").indexOf(tentativa[i])+" --- Erro")
+					this.tabuleiro[this.linha][i].classe = "erro";
+				}	
 			}
 		}
 		if(ganhou == 5){
-			this.mensagem = "GANHOU! A Palavra era "+this.palavraAdivinhar+"!";
+			this.mensagem = "Descobriu!";
+			this.linha = 7;
 		}else{
 			if(this.linha < 6){
+				this.linha += 1;
+				this.coluna = 0;
+				this.palavraUser = "";
+			}else{
 				this.mensagem = "Não foi desta vez, mais sorte na próxima! A Palavra era "+this.palavraAdivinhar+".";
 			}
 		}
-
-		// inicia loop
-		// for (let i = 0; i < 5; i++){
-		// 	//se a letra da coluna da palavra do usuario for a mesma letra da mesma coluna da palavra chave
-		// 	if (this.palavraUser.charAt(i) == this.palavraAdivinhar.charAt(i)){
-		// 		//se sim, classifica a letra como um acerto, cinco acertos ganha o jogo
-		// 		this.tabuleiro[this.linha][i].classe = "acerto";
-		// 		this.ganhou++;
-		// 		if(this.ganhou==5){
-		// 			mensagem = "GANHOU! A Palavra era "+this.palavraAdivinhar+"!";
-		// 			break;
-		// 		}
-		// 	//se nao
-		// 	}else{
-		// 		//checa se a letra existe na palavra
-		// 		if(this.palavraAdivinhar.indexOf(this.palavraUser.charAt(i)) > -1){
-		// 			//se sim, checa se só existe uma daquela letra na palavra
-		// 			if(this.palavraAdivinhar.indexOf(this.palavraUser.charAt(i))== 
-		// 				this.palavraAdivinhar.lastIndexOf(this.palavraUser.charAt(i))){
-		// 				//se for uma letra soh, classifica letra como no lugar errado
-		// 					this.tabuleiro[this.linha][i].classe = "lugarErrado";
-		// 			}else{
-		// 			}
-		// 		//se não, a letra esta errada
-		// 		}else{ 
-		// 			this.tabuleiro[this.linha][i].classe = "erro";
-		// 		}
-		// 	}
-		// }
-		// 	if(this.linha==5){
-		// 		alert("todas as tentativas usadas");
-		// 	}else{
-		// 		this.ganhou = 0;
-		// 		this.linha += 1;
-		// 		this.coluna = 0;
-		// 		this.palavraUser = "";
-			
-		// }
 	}
 
 	constructor() { }
