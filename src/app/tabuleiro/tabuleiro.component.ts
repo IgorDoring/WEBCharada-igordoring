700690f3962c94d3b7244ee4c8ad83b51b5a8e63
IgorDoring <idoring@tutanota.com>
2022-09-15 04:31:23 -0300

    lógica teclado

--- src/app/tabuleiro/tabuleiro.component.ts
+++ src/app/tabuleiro/tabuleiro.component.ts
@@ -14,9 +14,7 @@ export class TabuleiroComponent implements OnInit {
 	palavraAdivinhar: string = "ASSAR";
 	mensagem: any = {classe: "", text: ""};
 	acertou: boolean = false;
-	primeiraRowTeclado: string[] = ['Q','W','E','R','T','Y','U','I','O','P'];
-	segundaRowTeclado: string[] = ['Q','W','E','R','T','Y','U','I','O','P'];
-	terceiraRowTeclado: string[] = ['Q','W','E','R','T','Y','U','I','O','P'];
+	letrasVerificadas: any[] = [{letra: "", classe: ""}];
 
 	tabuleiro: any[][] = [
 	[{letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}],
@@ -27,8 +25,13 @@ export class TabuleiroComponent implements OnInit {
 	[{letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}]
 	];
 
-	montarIndex(linha: any, id: any): string {
-		return linha+"-"+id;
+	checkLetra(tecla: string): string {
+		let classe = "";
+		for(let i = 0; i < this.letrasVerificadas.length; i++){
+			if(this.letrasVerificadas[i].letra == tecla)
+				classe = this.letrasVerificadas[i].classe;
+		}
+		return classe;
 	}
 
 	getLetra(letra: string) : void {
@@ -58,6 +61,7 @@ export class TabuleiroComponent implements OnInit {
 			if(tentativa[i]==resposta[i]){
 				this.tabuleiro[this.linha][i].classe = "acerto";
 				ganhou++;
+				this.letrasVerificadas.push({letra: tentativa[i], classe: "acerto"});
 				resposta[i] = " ";
 				if(ganhou == 5){
 					this.mensagem.classe = "acerto";
@@ -68,8 +72,10 @@ export class TabuleiroComponent implements OnInit {
 			}else{
 				if(resposta.join("").indexOf(tentativa[i]) > -1){
 					this.tabuleiro[this.linha][i].classe = "lugarErrado";
+				this.letrasVerificadas.push({letra: tentativa[i], classe: "lugarErrado"});
 					resposta[resposta.join("").indexOf(tentativa[i])] = " ";
 				}else{
+					this.letrasVerificadas.push({letra: tentativa[i], classe: "erro"});
 					this.tabuleiro[this.linha][i].classe = "erro";
 				}	
 			}
@@ -78,6 +84,8 @@ export class TabuleiroComponent implements OnInit {
 			this.linha += 1;
 			this.coluna = 0;
 			this.palavraUser = "";
+			this.mensagem.classe = "aviso";
+			this.mensagem.text = "Errado! Tente novamente.";
 		}else{
 			this.mensagem.classe = "erro";
 			this.mensagem.text = "Não foi desta vez, mais sorte na próxima! A Palavra era "+this.palavraAdivinhar+".";
