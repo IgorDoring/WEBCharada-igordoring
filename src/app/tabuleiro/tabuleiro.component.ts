e9f28d3bd92f6e2503914dceb9c790f19a2f70c4
IgorDoring <idoring@tutanota.com>
2022-09-18 18:30:25 -0300

    minor fix logica e teclado do tabuleiro

--- src/app/tabuleiro/tabuleiro.component.ts
+++ src/app/tabuleiro/tabuleiro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TabuleiroService} from "../tabuleiro.service";
 
 @Component({
 	selector: 'app-tabuleiro',
@@ -7,92 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class TabuleiroComponent implements OnInit {
+	linha: number;
+	coluna: number;
+	palavraUser: string;
+	palavraAdivinhar: string;
+	letrasVerificadas: any[];
+	mensagem: any;
+	terminaJogo: boolean;
+	tabuleiro: any[][];
 
-	linha: number = 0;
-	coluna: number = 0;
-	palavraUser: string = "";
-	palavraAdivinhar: string = "ASSAR";
-	mensagem: any = {classe: "", text: ""};
-	acertou: boolean = false;
-	letrasVerificadas: any[] = [{letra: "", classe: ""}];
-
-	tabuleiro: any[][] = [
-	[{letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}],
-	[{letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}],
-	[{letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}],
-	[{letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}],
-	[{letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}],
-	[{letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}, {letra: "", classe: ""}]
-	];
-
-	checkLetra(tecla: string): string {
-		let classe = "";
-		for(let i = 0; i < this.letrasVerificadas.length; i++){
-			if(this.letrasVerificadas[i].letra == tecla)
-				classe = this.letrasVerificadas[i].classe;
-		}
-		return classe;
+	resetGame(){
+		this.tabuleiroService.resetGame();
 	}
 
-	getLetra(letra: string) : void {
-		if (this.coluna<5&&!this.acertou){
-			this.tabuleiro[this.linha][this.coluna].letra = letra;
-			this.coluna += 1;
-			this.palavraUser = this.palavraUser + letra.toUpperCase();
-		}
+	checkLetra(tecla: string){
+		return this.tabuleiroService.checkLetra(tecla);
 	}
 
-	apagarLetra(): void{
-		if(this.coluna > 0&&!this.acertou){
-			this.coluna -= 1;
-			this.tabuleiro[this.linha][this.coluna].letra = "";
-			this.palavraUser = this.palavraUser.slice(0, this.coluna);
-		}
+	getLetra(letra: string){
+		this.tabuleiroService.getLetra(letra);
 	}
 
-	adivinhar(tentativa: string[] = this.palavraUser.split(""), resposta: string[] = this.palavraAdivinhar.split("")): void {
-		let ganhou: number = 0;
-		if(this.coluna < 5){
-			this.mensagem.classe = "aviso";
-			this.mensagem.text = "É obrigatório conter 5 letras.";
-			return;
-		}
-		for(let i = 0; i < 5; i++){
-			if(tentativa[i]==resposta[i]){
-				this.tabuleiro[this.linha][i].classe = "acerto";
-				ganhou++;
-				this.letrasVerificadas.push({letra: tentativa[i], classe: "acerto"});
-				resposta[i] = " ";
-				if(ganhou == 5){
-					this.mensagem.classe = "acerto";
-					this.mensagem.text = "Descobriu!";
-					this.acertou = true;
-					return;
-				}
-			}else{
-				if(resposta.join("").indexOf(tentativa[i]) > -1){
-					this.tabuleiro[this.linha][i].classe = "lugarErrado";
-				this.letrasVerificadas.push({letra: tentativa[i], classe: "lugarErrado"});
-					resposta[resposta.join("").indexOf(tentativa[i])] = " ";
-				}else{
-					this.letrasVerificadas.push({letra: tentativa[i], classe: "erro"});
-					this.tabuleiro[this.linha][i].classe = "erro";
-				}	
-			}
-		}
-		if(this.linha < 5){
-			this.linha += 1;
-			this.coluna = 0;
-			this.palavraUser = "";
-			this.mensagem.classe = "aviso";
-			this.mensagem.text = "Errado! Tente novamente.";
-		}else{
-			this.mensagem.classe = "erro";
-			this.mensagem.text = "Não foi desta vez, mais sorte na próxima! A Palavra era "+this.palavraAdivinhar+".";
-		}
+	apagarLetra(){
+		this.tabuleiroService.apagarLetra();
 	}
 
-	constructor() { }
+	adivinhar(){
+		this.tabuleiroService.adivinhar();
+	}
+
+	constructor(public tabuleiroService: TabuleiroService) { 
+		this.linha = this.tabuleiroService.linha;
+		this.coluna = this.tabuleiroService.coluna;
+		this.palavraUser = this.tabuleiroService.palavraUser;
+		this.palavraAdivinhar = this.tabuleiroService.palavraAdivinhar;
+		this.letrasVerificadas = this.tabuleiroService.letrasVerificadas;
+		this.mensagem = this.tabuleiroService.mensagem;
+		this.terminaJogo= this.tabuleiroService.terminaJogo;
+		this.tabuleiro = this.tabuleiroService.tabuleiro;
+
+	}
 
 	ngOnInit(): void {
 
